4b1dc19bdd7c16b532e11fc43a306a1ff8d4bff4
Warp <warpaboi@gmail.com>
2023-11-14 14:51:41 +0100

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,86 @@
+
+export interface Env {
+    x57_bucket: R2Bucket;
+    x57_db: KVNamespace;
+}
+
+export default {
+    async fetch(
+        request: Request,
+        env: Env) {
+        const url = new URL(request.url);
+
+        switch (request.method) {
+            case 'POST':
+                let token = url.searchParams.get('token');
+                if (token === null) {
+                    console.log('no token');
+                    return new Response('', { status: 401 });
+                }
+
+                let tokencsv = await env.x57_db.get('_tokens');
+                if (tokencsv === null) {
+                    console.log('no tokens');
+                    return new Response('', { status: 401 });
+                }
+
+                let tokens = tokencsv.split(',');
+                if (!tokens.includes(token)) {
+                    console.log('bad token');
+                    return new Response('', { status: 401 });
+                }
+
+
+                let rng = crypto.getRandomValues(new Uint8Array(4));
+                let rngName = [...new Uint8Array(rng)]
+                    .map((b) => b.toString(36).padStart(2, '0'))
+                    .join('')
+                    .substring(0, 4);
+
+                await env.x57_bucket.put(rngName, request.body);
+
+                if (url.searchParams.get('name') !== null) {
+                    await env.x57_db.put(rngName, url.searchParams.get('name')!);
+                }
+
+                return new Response(url.origin + '/' + rngName);
+
+            case 'GET':
+                let key = url.pathname.slice(1);
+                let object = await env.x57_bucket.get(key);
+
+                if (object === null) {
+                    return new Response('', { status: 404 });
+                }
+
+                let name = await env.x57_db.get(key);
+                if (name === null) {
+                    var mime = require('mime-types')
+                    var ext = mime.extension(object?.httpMetadata?.contentType)
+                    if (ext === false) {
+                        name = key
+                    }
+                    else {
+                        name = key + '.' + ext
+                    }
+                }
+
+                let headers = new Headers();
+                object.writeHttpMetadata(headers);
+                headers.set('etag', object.httpEtag);
+                headers.set('content-disposition', 'inline; filename="' + name + '"');
+
+                return new Response(object.body, {
+                    headers,
+                });
+
+            default:
+                return new Response(`${request.method} is not allowed.`, {
+                    status: 405,
+                    headers: {
+                        Allow: 'POST, GET',
+                    },
+                });
+        }
+    },
+};
